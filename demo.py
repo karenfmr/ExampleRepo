d57fdf2ba0ab6e67eef628925e2195876d32b981
Karen Martinez <karenmartinez@Karens-MacBook-Air.local>
2019-07-29 10:27:36 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,15 @@
+# -------------------------------------------------
+#        Name: <YOUR NAME HERE>
+#    Filename: lab2.py
+#        Date: <TODAY'S DATE HERE>
+#
+# Description: <include a description
+#               of what the file contains>
+# -------------------------------------------------
+def main ():
+    name="Karen Martinez"
+    print("Hello my name is", name)
+
+if __name__=="__main__":
+    main()
+#references
